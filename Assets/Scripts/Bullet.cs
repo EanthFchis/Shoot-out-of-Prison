cf659fa3ca8db1b54ef6f37d9331d140cad59399
EanthFchis <76567513+EanthFchis@users.noreply.github.com>
2021-06-26 11:50:20 -0400

    Initial check-in of module Shoot out of Prison

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject hitEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        ParticleSystem hitEffectParticleSystem = hitEffect.GetComponent<ParticleSystem>();
+        hitEffectParticleSystem.Play();
+        Destroy(effect, 2f);
+        Destroy(gameObject);
+    }
+
+}
